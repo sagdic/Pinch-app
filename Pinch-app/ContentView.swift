b801c4bf0838f62462beb20509b47b565276c1e1
Tayfun Sağdıç <30289904+sagdic@users.noreply.github.com>
2022-06-10 10:21:08 +0300

    Initial Commit

---
+++ Pinch-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pinch-app
+//
+//  Created by tayfun on 10.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pinch-app/Pinch_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_appApp.swift
+//  Pinch-app
+//
+//  Created by tayfun on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
